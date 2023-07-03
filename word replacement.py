a0af4107d71e0621bd6e5b331dc3e172bfde7ee5
Rupesh <98076862+Susrobin21@users.noreply.github.com>
2023-07-03 17:59:53 +0530

    Update word replacement.py

--- word replacement.py
+++ word replacement.py
@@ -1,6 +1,6 @@
 #word replacement project 
 def replacee():
-    stat = 'hiee this is my first project, well technically speaking this cant even be called a project but yes i will try to make better ones'
+    stat = input('enter a string :')
     strrepd = input('enter the word to be replaced :')
     strepct = input('enter the replacement word :')
     swap = stat.replace(strrepd,strepct)
