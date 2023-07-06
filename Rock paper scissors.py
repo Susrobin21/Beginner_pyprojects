3c3df4ee3d18a374d49e450599cdb06b932fab71
Rupesh <98076862+Susrobin21@users.noreply.github.com>
2023-07-06 20:27:34 +0530

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -52,11 +52,11 @@ while exit== False:
             user_points +=1
         elif computer_choice =='P' or computer_choice == 'P' :
             print('Your input and the computer input are the same :( ')
-
-
-# i still have a few corrections in the code regarding the exit part it's not so effecient but i'm working on it .
             print('Tie it is then !')
 
     elif user_choice != 'r' or user_choice != 'R' or user_choice != 'p' or user_choice != 'P' or user_choice != 's' or user_choice != 'S'  or user_choice != 'exit':
         print('Invalid input, please choose out of the below options :')
         print(' For rock choose r or R\n For scissors choose s or S\n For paper choose p or P\n To exit type exit please')
+
+
+# i still have a few corrections in the code regarding the exit part it's not so effecient but i'm working on it .
